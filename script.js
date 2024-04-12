0492b031c46a770532aa435dce221a3e7db535b4
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-04-12 08:23:05 +0200

    Rename 'addition' to add and 'subtraction' to subtract functions

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const addition = function(a, b) {
+const add = function(a, b) {
 	return a + b;
 };
 
-const subtraction = function(a, b) {
+const subtract = function(a, b) {
 	return a - b;
 };
 
 const multiply = function(a, b) {
 	return a * b;
 }
-
-console.log(multiply(4, 4)); // 16
