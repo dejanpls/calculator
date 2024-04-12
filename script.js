b87bd6470750a1baead7ba41915afd1fe39582b7
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-04-12 08:21:49 +0200

    Create a new function 'multiply'

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const addition = function(a, b) {
 const subtraction = function(a, b) {
 	return a - b;
 };
+
+const multiply = function(a, b) {
+	return a * b;
+}
+
+console.log(multiply(4, 4)); // 16
