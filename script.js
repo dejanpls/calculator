c248de31e30c77301d5172f3a34123fabb920d68
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-04-12 08:38:30 +0200

    Create a new function operate
    	What was done:
    		- 'operate' takes an operator and 2 numbers,
    		- then calls one of the functions on the numbers

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ const subtract = function(a, b) {
 
 const multiply = function(a, b) {
 	return a * b;
-}
+};
+
+const divide = function(a, b) {
+	return a / b;
+};
+
+let operandA;
+let operandB;
+let operator;
+
+const operate = function (operandA, operandB, operator) {
+	return operator(operandA, operandB);
+};
