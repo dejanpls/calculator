f04468cd60464b1470d9373f613ccbf8d0e4a525
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-04-25 09:18:38 +0200

    Code cleanup: Reorder functions according to their execution
    	What was done:
    		- combine getKeyOperand() and getOperands() functions into getOperands()
    		- reorder functions according to their execution

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ absoluteBtn.addEventListener("mousedown", getAbsolute);
 percentageBtn.addEventListener("mousedown", getPercentage);
 undoBtn.addEventListener("mousedown", undoLastNumber);
 
-document.addEventListener("keydown", getKeyOperand);
+document.addEventListener("keydown", getOperands);
 document.addEventListener("keydown", undoKey);
 document.addEventListener("keydown", getKeyOperator);
 document.addEventListener("keydown", evaluateKey);
@@ -47,15 +47,19 @@ let operandB = "";
 let operator;
 
 function getOperands(e) {
-	addValue(e.target);
-}   
+	if (e.key) {
+		const key = document.querySelector(`button.integer[key="${e.key}"]`)
+		if (!key) return;
+		addToOperand(key);
 
-function addValue(value) {
+	} else addToOperand(e.target);
+}
+
+function addToOperand(value) {
 
 	if (!(display.textContent.includes(".") && value.textContent == ".")) {
     	if (operator === "equalize") {
-      		operandA = value.textContent;
-    		operandB = "";
+      		saveToOperandA(value);
     		operator = undefined;
       	} 
     	else if (!operator && operandA.length < 9) operandA += value.textContent;       	
@@ -74,10 +78,15 @@ function getOperator(e) {
     if (!operandB) if (e.target.classList[2] !== "equalize") operator = e.target.classList[2];
 }
 
+function getKeyOperator(e) {
+	const key = document.querySelector(`button.operator[key="${e.key}"]`)
+	if (!key) return;
+	if (!operandB) if (key.classList[2] !== "equalize") operator = key.classList[2];
+}
+
 function evaluate(e) {
     if (operandA && operandB) {
         let result = operate (operandA, operandB, window[operator]);
-
         if (result === Infinity) {
         	clearAll();
         	display.textContent = "NO WAY JOSE";
@@ -89,9 +98,19 @@ function evaluate(e) {
     }
 }
 
+function evaluateKey(e) {
+	const key = document.querySelector(`button.operator[key="${e.key}"]`)
+	if (!key) return;
+	evaluate(key);
+}
+
+function saveToOperandA(content) {
+	operandA = content.textContent;
+	operandB = "";
+}
+
 function saveResult(e) {
-    operandA = display.textContent;
-    operandB = "";
+	saveToOperandA(display);
     if (e.target) operator = e.target.classList[2];
     else operator = e.classList[2];
 }
@@ -106,17 +125,13 @@ function clearAll() {
 function getAbsolute() {
 	if (display.textContent < 0) display.textContent = Math.abs(display.textContent)
 	else display.textContent = -Math.abs(display.textContent);
-
-	operandA = display.textContent;
-    operandB = "";
+	saveToOperandA(display);
 }
 
 function getPercentage() {
 	if (!operandB) display.textContent = parseFloat(display.textContent) * 0.01;
 	else display.textContent = parseFloat(operandA) * parseFloat(operandB) * 0.01;
-
-	operandA = display.textContent;
-	operandB = "";
+	saveToOperandA(display);
 }
 
 function undoLastNumber() {
@@ -132,27 +147,8 @@ function undoLastNumber() {
 	}
 }
 
-// Keyboard Functions
-function getKeyOperand(e) {
-	const key = document.querySelector(`button.integer[key="${e.key}"]`)
-	if (!key) return;
-	addValue(key);
-}
-
 function undoKey(e) {
 	const key = document.querySelector(`button.undo[key="${e.key}"]`)
 	if (!key) return;
 	undoLastNumber();
-}
-
-function getKeyOperator(e) {
-	const key = document.querySelector(`button.operator[key="${e.key}"]`)
-	if (!key) return;
-	if (!operandB) if (key.classList[2] !== "equalize") operator = key.classList[2];
-}
-
-function evaluateKey(e) {
-	const key = document.querySelector(`button.operator[key="${e.key}"]`)
-	if (!key) return;
-	evaluate(key);
 }
